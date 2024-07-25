2bf25447a50c8e4a8928cf85c1a203830196d3a2
Ricardo <ricardowoj@hotmail.com>
2024-07-25 09:12:13 +0100

    add basic project

---
+++ src/test/java/com/ricardow/csv_tools/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ricardow.csv_tools.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
